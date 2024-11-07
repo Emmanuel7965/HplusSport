7bd0d63ceb27833ded428cc198ca6c205c63ef66
Emmanuel796501~ <ajayie993@gmail.com>
2024-11-07 10:44:17 +0100

    ff

--- HPlusSport.API/Controllers/ProductController.cs
+++ HPlusSport.API/Controllers/ProductController.cs
@@ -1,16 +1,38 @@
+﻿
+﻿using HPlusSport.API.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace HPlusSport.API.Controllers{
-     [Route("api/[controller]")]
-     [ApiController]
-     public class ProductsController : ControllerBase
-     {
-        [HttpGet]
-        public string GetProducts()
+namespace HPlusSport.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ShopContext _context;
+
+        public ProductsController(ShopContext context)
         {
-            return "ok";
+            _context = context;
+
+            _context.Database.EnsureCreated();
         }
 
-     }
+        [HttpGet]
+        public ActionResult GetAllProducts()
+        {
+            return Ok(_context.Products.ToArray());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetProduct(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
 }
